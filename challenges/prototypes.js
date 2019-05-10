0e8b881255f2966144d6fa4ec85492c45c5ecd6b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-10 09:31:45 +0000

    chore: Change let to const as variable never gets reassigned

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -18,7 +18,7 @@ function CuboidMaker(props) {
   Formula for cuboid volume: length * width * height
 */
 CuboidMaker.prototype.volume = function() {
-  let volume = this.length * this.width * this.height;
+  const volume = this.length * this.width * this.height;
   console.log(`Cuboid's volume is - ${volume}`);
 }
 

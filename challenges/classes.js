99e90aff6c7d60c8c7623e4c6ffe5f2e1c246839
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-10 08:59:44 +0000

    feature: Refactor CuboidMaker to CuboidMakerClass with the class keyword

--- challenges/classes.js
+++ challenges/classes.js
@@ -2,10 +2,18 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-function CuboidMaker(props) {
-  this.length = props.length;
-  this.width = props.width;
-  this.height = props.height;
+// function CuboidMaker(props) {
+//   this.length = props.length;
+//   this.width = props.width;
+//   this.height = props.height;
+// }
+
+class CuboidMakerClass {
+  constructor(props) {
+    this.length = props.length;
+    this.width = props.width;
+    this.height = props.height;
+  }
 }
 
 
